786370fff48933380d641f53b86e76b000869355
janithRS <janithravindu@gmail.com>
2020-03-15 23:14:56 +0530

    Remove logs, rename 'message' to 'payload' and remove white spaces in app.js

--- live/app.js
+++ live/app.js
@@ -1,77 +1,70 @@
 function connect() {
-    let stompClient = null;
-    let socket = new SockJS('https://sack-live-score.herokuapp.com/live'); //todo :
-    stompClient = Stomp.over(socket);
+    const socket = new SockJS('https://sack-live-score.herokuapp.com/live'); //todo :
+    const stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
-        console.log('Connected: ' + frame);
         stompClient.subscribe('/topic/test-match', function (match) {
             setValues(JSON.parse(match.body));
-
         });
     });
 }
 
-function setValues(message) {
-    console.log(message);
-
+function setValues(payload) {
     //team one set values
-    $("#team-one-first-innings-score").text(message.teamOneFirstInning.score);
-    $("#team-one-first-innings-wickets").text(message.teamOneFirstInning.wickets);
-    $("#team-one-first-innings-overs").text(message.teamOneFirstInning.overs);
-    $("#team-one-first-innings-balls").text(message.teamOneFirstInning.balls);
-    $("#team-one-first-innings-player-one").text(message.teamOneFirstInning.playerOne.name);
-    $("#team-one-first-innings-player-two").text(message.teamOneFirstInning.playerTwo.name);
-    if (message.teamOneFirstInning.playerOne.isBatting === true && message.teamOneFirstInning.playerTwo.isBatting === false) {
+    $("#team-one-first-innings-score").text(payload.teamOneFirstInning.score);
+    $("#team-one-first-innings-wickets").text(payload.teamOneFirstInning.wickets);
+    $("#team-one-first-innings-overs").text(payload.teamOneFirstInning.overs);
+    $("#team-one-first-innings-balls").text(payload.teamOneFirstInning.balls);
+    $("#team-one-first-innings-player-one").text(payload.teamOneFirstInning.playerOne.name);
+    $("#team-one-first-innings-player-two").text(payload.teamOneFirstInning.playerTwo.name);
+    if (payload.teamOneFirstInning.playerOne.isBatting === true && payload.teamOneFirstInning.playerTwo.isBatting === false) {
         $('#team-one-first-innings-player-one-batting').show();
         $('#team-one-first-innings-player-two-batting').hide();
     }
-    if (message.teamOneFirstInning.playerTwo.isBatting === true && message.teamOneFirstInning.playerOne.isBatting === false) {
+    if (payload.teamOneFirstInning.playerTwo.isBatting === true && payload.teamOneFirstInning.playerOne.isBatting === false) {
         $('#team-one-first-innings-player-one-batting').hide();
         $('#team-one-first-innings-player-two-batting').show();
     }
-
-    $("#team-one-second-innings-score").text(message.teamOneSecondInning.score);
-    $("#team-one-second-innings-wickets").text(message.teamOneSecondInning.wickets);
-    $("#team-one-second-innings-overs").text(message.teamOneSecondInning.overs);
-    $("#team-one-second-innings-balls").text(message.teamOneSecondInning.balls);
-    $("#team-one-second-innings-player-one").text(message.teamOneSecondInning.playerOne.name);
-    $("#team-one-second-innings-player-two").text(message.teamOneSecondInning.playerTwo.name);
-    if (message.teamOneSecondInning.playerOne.isBatting === true && message.teamOneSecondInning.playerTwo.isBatting === false) {
+    $("#team-one-second-innings-score").text(payload.teamOneSecondInning.score);
+    $("#team-one-second-innings-wickets").text(payload.teamOneSecondInning.wickets);
+    $("#team-one-second-innings-overs").text(payload.teamOneSecondInning.overs);
+    $("#team-one-second-innings-balls").text(payload.teamOneSecondInning.balls);
+    $("#team-one-second-innings-player-one").text(payload.teamOneSecondInning.playerOne.name);
+    $("#team-one-second-innings-player-two").text(payload.teamOneSecondInning.playerTwo.name);
+    if (payload.teamOneSecondInning.playerOne.isBatting === true && payload.teamOneSecondInning.playerTwo.isBatting === false) {
         $('#team-one-second-innings-player-one-batting').show();
         $('#team-one-second-innings-player-two-batting').hide();
     }
-    if (message.teamOneSecondInning.playerTwo.isBatting === true && message.teamOneSecondInning.playerOne.isBatting === false) {
+    if (payload.teamOneSecondInning.playerTwo.isBatting === true && payload.teamOneSecondInning.playerOne.isBatting === false) {
         $('#team-one-second-innings-player-one-batting').hide();
         $('#team-one-second-innings-player-two-batting').show();
     }
 
     //team two set values
-    $("#team-two-first-innings-score").text(message.teamTwoFirstInning.score);
-    $("#team-two-first-innings-wickets").text(message.teamTwoFirstInning.wickets);
-    $("#team-two-first-innings-overs").text(message.teamTwoFirstInning.overs);
-    $("#team-two-first-innings-balls").text(message.teamTwoFirstInning.balls);
-    $("#team-two-first-innings-player-one").text(message.teamTwoFirstInning.playerOne.name);
-    $("#team-two-first-innings-player-two").text(message.teamTwoFirstInning.playerTwo.name);
-    if (message.teamTwoFirstInning.playerOne.isBatting === true && message.teamTwoFirstInning.playerTwo.isBatting === false) {
+    $("#team-two-first-innings-score").text(payload.teamTwoFirstInning.score);
+    $("#team-two-first-innings-wickets").text(payload.teamTwoFirstInning.wickets);
+    $("#team-two-first-innings-overs").text(payload.teamTwoFirstInning.overs);
+    $("#team-two-first-innings-balls").text(payload.teamTwoFirstInning.balls);
+    $("#team-two-first-innings-player-one").text(payload.teamTwoFirstInning.playerOne.name);
+    $("#team-two-first-innings-player-two").text(payload.teamTwoFirstInning.playerTwo.name);
+    if (payload.teamTwoFirstInning.playerOne.isBatting === true && payload.teamTwoFirstInning.playerTwo.isBatting === false) {
         $('#team-one-first-innings-player-one-batting').show();
         $('#team-two-first-innings-player-two-batting').hide();
     }
-    if (message.teamTwoFirstInning.playerTwo.isBatting === true && message.teamTwoFirstInning.playerOne.isBatting === false) {
+    if (payload.teamTwoFirstInning.playerTwo.isBatting === true && payload.teamTwoFirstInning.playerOne.isBatting === false) {
         $('#team-two-first-innings-player-one-batting').hide();
         $('#team-two-first-innings-player-two-batting').show();
     }
-
-    $("#team-two-second-innings-score").text(message.teamTwoSecondInning.score);
-    $("#team-two-second-innings-wickets").text(message.teamTwoSecondInning.wickets);
-    $("#team-two-second-innings-overs").text(message.teamTwoSecondInning.overs);
-    $("#team-two-second-innings-balls").text(message.teamTwoSecondInning.balls);
-    $("#team-two-second-innings-player-one").text(message.teamTwoSecondInning.playerOne.name);
-    $("#team-two-second-innings-player-two").text(message.teamTwoSecondInning.playerTwo.name);
-    if (message.teamTwoSecondInning.playerOne.isBatting === true && message.teamTwoSecondInning.playerTwo.isBatting === false) {
+    $("#team-two-second-innings-score").text(payload.teamTwoSecondInning.score);
+    $("#team-two-second-innings-wickets").text(payload.teamTwoSecondInning.wickets);
+    $("#team-two-second-innings-overs").text(payload.teamTwoSecondInning.overs);
+    $("#team-two-second-innings-balls").text(payload.teamTwoSecondInning.balls);
+    $("#team-two-second-innings-player-one").text(payload.teamTwoSecondInning.playerOne.name);
+    $("#team-two-second-innings-player-two").text(payload.teamTwoSecondInning.playerTwo.name);
+    if (payload.teamTwoSecondInning.playerOne.isBatting === true && payload.teamTwoSecondInning.playerTwo.isBatting === false) {
         $('#team-two-second-innings-player-one-batting').show();
         $('#team-two-second-innings-player-two-batting').hide();
     }
-    if (message.teamTwoSecondInning.playerTwo.isBatting === true && message.teamTwoSecondInning.playerOne.isBatting === false) {
+    if (payload.teamTwoSecondInning.playerTwo.isBatting === true && payload.teamTwoSecondInning.playerOne.isBatting === false) {
         $('#team-two-second-innings-player-one-batting').hide();
         $('#team-two-second-innings-player-two-batting').show();
     }
